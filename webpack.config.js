42f5e3368d5a3d134cf1d1c481ae89ba2927d2cf
Daichi Kimura <kimura@feedforce.jp>
2016-07-04 18:38:58 +0900

    Fixed ESLint Errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
-/* eslint-disable no-var */
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  debug:   true,
-  entry:   {
+  debug: true,
+  entry: {
     app: [
       'webpack/hot/dev-server',
       'webpack-hot-middleware/client',
-      './src/js/app.js'
-    ]
+      './src/js/app.js',
+    ],
   },
-  output:  {
-    path:       '/',
-    filename:   '[name].bundle.js',
-    publicPath: 'http://localhost:3000/js/'
+  output: {
+    path: '/',
+    filename: '[name].bundle.js',
+    publicPath: 'http://localhost:3000/js/',
   },
   devtool: 'inline-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
-  module:  {
-    loaders: [
-      {test: /\.jsx?$/, loader: 'babel', include: path.join(__dirname, './src/js')}
-    ]
-  }
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: path.join(__dirname, './src/js'),
+    }],
+  },
 };
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,30 +1,33 @@
-/* eslint-disable no-var */
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry:   {
-    app: ['./src/js/app.js']
+  entry: {
+    app: ['./src/js/app.js'],
   },
-  output:  {
-    path:     path.join(__dirname, './dist/'),
-    filename: '/js/[name].bundle.js'
+  output: {
+    path: path.join(__dirname, './dist/'),
+    filename: '/js/[name].bundle.js',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {'NODE_ENV': JSON.stringify('production')}
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         drop_console: true,
-        warnings:     false
-      }
-    })
+        warnings: false,
+      },
+    }),
   ],
-  module:  {
-    loaders: [
-      {test: /\.jsx?$/, loader: 'babel', include: path.join(__dirname, './src/js')}
-    ]
-  }
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: path.join(__dirname, './src/js'),
+    }],
+  },
 };
